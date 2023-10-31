59de993449f84f615e4739b426ce2157d2ad7285
Shuhan Jin <sjin9@slb.com>
2023-10-31 16:16:55 +0800

    feat(blog): implement the functionallity of generating multiplication table

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,63 @@
+
+export type Multiplication = [number, number, number] //define a 数据结构
+
 export class MultiplicationTable {
-  public render(start: number, end: number): string{
-    return '1*1=1'
+  public renderMultiplicationTable(start: number, end: number): string | null{ //string or null
+    if(!this.isValid(start,end)) {
+      return null
+    }
+
+    const table: Multiplication[][] = this.generateMultiplicationTable(start,end)
+
+    const tableString: string = this.render(table)
+    return tableString
   }
+
+  private isValid(start: number, end: number): boolean {
+    return Number.isInteger(start)&&Number.isInteger(end) //make sure both values are integer
+    &&this.isValidNumber(start)&&this.isValidNumber(end)
+    &&this.isEndGreaterOrEqualToStart(start, end)
+    //all set as provate unless要实现这个方法，作用域尽量小
+    //use this. in private method
+  }
+
+  private isValidNumber(num: number): boolean {
+    return num >=1 && num <= 1000
+  }
+
+  private isEndGreaterOrEqualToStart(start:number, end: number) {
+    return start <= end
+  }
+
+  private generateSingleMultiplication(start:number, end: number): number {
+    const product = start*end
+    return product
+  }
+
+  private generateRowMultiplier(start:number, end: number): Multiplication[] {
+    const row:Multiplication[] = []
+    for(let i=start; i<=end; i++) {
+      const productInRow = this.generateSingleMultiplication(i, end)
+      row.push([i, end, productInRow])
+    }
+    return row
+  }
+
+  private generateMultiplicationTable(start:number, end: number):Multiplication[][] {
+    const table:Multiplication[][] = []
+    for(let j=start; j<=end; j++) {
+      const column = this.generateRowMultiplier(start, j)
+      table.push(column)
+    }
+    return table
+  }
+
+  private render(table: Multiplication[][]):string{
+    return table.map(line=>
+      line.map(
+        item => `${item[0]} * ${item[1]} = ${item[2]}`) //模板字符串
+        .join('  ')
+    ).join('\n')
+  }
+
 }
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -8,9 +8,42 @@ describe('Multiplication Table', () => {
     const end = 1
 
     //when
-    const rendered = table.render(start, end)
+    const rendered = table.renderMultiplicationTable(start, end)
 
     //then
-    expect(rendered).toBe('1*1=1')
+    expect(rendered).toBe('1 * 1 = 1')
   })
+  it('should return null for invalid inputs', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 4
+    const end = 2
+    // when
+    const rendered = table.renderMultiplicationTable(start, end)
+    // then
+    expect(rendered).toBeNull()
+  })
+
+  it('should return a multuplication table between 2 and 4', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 2
+    const end = 3
+    // when
+    const rendered = table.renderMultiplicationTable(start, end)
+    // then
+    expect(rendered).toBe('2 * 2 = 4\n2 * 3 = 6  3 * 3 = 9')
+  })
+
+  it('should return null for non-integer value', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 4.444
+    const end = 9
+    // when
+    const rendered = table.renderMultiplicationTable(start, end)
+    // then
+    expect(rendered).toBeNull()
+  })
+
 })
